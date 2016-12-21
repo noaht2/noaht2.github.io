9530f921bc7bef82b96886656d4f6233f277452e
noaht2 <noahtarr17@gmail.com>
2016-12-21 14:18:48 +0000

    Update interactive.js

--- js/interactive.js
+++ js/interactive.js
@@ -1,2 +1,3 @@
-alert("You are now entering the world of javaScript. You are advised to hang tight.")
-prompt("What do you want to appear?")
+function main (request) {
+  alert("You are now entering the world of javaScript. You are advised to hang tight.")
+  var request=prompt("What do you want to appear?")
