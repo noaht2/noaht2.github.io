af08a940058511f498ae093909e32823f2efa303
noaht2 <noahtarr17@gmail.com>
2016-12-22 13:17:01 +0000

    Update interactive.js

--- js/interactive.js
+++ js/interactive.js
@@ -1,6 +1,7 @@
 function main() {
   alert("You are now entering the world of javaScript. You are advised to hang tight.");
   $(prompt("Where do you want to add something")).text(prompt("What do you want to appear?"));
+  console.log("finished");
 }
 
 $(document).ready(main);
