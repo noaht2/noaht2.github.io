6f6ed816c70bf25da1c9305bec041e8e316abad7
Noah M. Tarr <noah@tarr.me.uk>
2025-02-08 18:02:06 +0000

    Clean up

--- README.md
+++
@@ -1,3 +0,0 @@
-# noaht2.github.io
-My personal website!
-[https://noaht2.github.io](https://noaht2.github.io)
--- js/1.js
+++
@@ -1,6 +0,0 @@
-function main() {
-  $(prompt("Where do you want to add something")).text(prompt("What do you want to appear?"));
-  console.log("finished");
-}
-
-$(document).ready(main);
